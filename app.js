b27cfb42eab63094a031114351dca4ff9f59c051
@networkpankaj <@networkpankaj>
2024-05-16 03:02:23 +0000

    Inititalizing

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require('express')
+
+const app = express();
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+const port= 3000
+
+app.listen(port,
+    console.log(`server is listening on port ${port}... `)
+)
--- public/browser-app.js
+++ public/browser-app.js
@@ -19,21 +19,18 @@ const showTasks = async () => {
       .map((task) => {
         const { completed, _id: taskID, name } = task
         return `<div class="single-task ${completed && 'task-completed'}">
-<h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
-<div class="task-links">
-
-
-
-<!-- edit link -->
-<a href="task.html?id=${taskID}"  class="edit-link">
-<i class="fas fa-edit"></i>
-</a>
-<!-- delete btn -->
-<button type="button" class="delete-btn" data-id="${taskID}">
-<i class="fas fa-trash"></i>
-</button>
-</div>
-</div>`
+          <h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
+          <div class="task-links">
+          <!-- edit link -->
+          <a href="task.html?id=${taskID}"  class="edit-link">
+          <i class="fas fa-edit"></i>
+          </a>
+          <!-- delete btn -->
+          <button type="button" class="delete-btn" data-id="${taskID}">
+          <i class="fas fa-trash"></i>
+          </button>
+          </div>
+          </div>`
       })
       .join('')
     tasksDOM.innerHTML = allTasks
