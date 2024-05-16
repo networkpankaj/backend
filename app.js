490f2c66e0daff647d2d6bbc229511acbcfc0d93
@networkpankaj <@networkpankaj>
2024-05-16 15:24:22 +0000

    fourth Init

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 
 const app = express();
+const tasks = require('./routes/tasks');
+
+//middleware
+
+app.use(express.json())
 
 //routes
 app.get('/hello', (req, res) => {
     res.send('Task Manager App')
 })
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')  - get all tasks
+// app.post('/api/v1/tasks')   - create a new tasks
+// app.get('/ap/v1/tasks/:id')   - get single task
+// app.patch('/ap/v1/tasks/:id')   - update task
+// app.delete('/ap/v1/tasks/:id')   - delete task
 
 const port= 3000
 
